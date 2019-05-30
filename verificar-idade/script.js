b6441525b6108534cd8a880123014f56a9bc5066
Marina Matos <45432113+marimatos@users.noreply.github.com>
2019-05-30 18:01:16 -0300

    Add files via upload

---
+++ verificar-idade/script.js
@@ -0,0 +1,44 @@
+function verificar(){
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = Number(document.getElementById('txtano').value)
+    var res = document.querySelector('div#res')
+
+    if (fano > ano) {
+        alert('Erro! Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - fano
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <= 10){
+                img.setAttribute('src', 'imagens/foto-bebe-f.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'imagens/foto-jovem-f.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/foto-adulto-f.png')
+            } else {
+                img.setAttribute('src', 'imagens/foto-idosa.png')
+            }
+        } else if (fsex[1].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <= 10){
+                img.setAttribute('src', 'imagens/foto-bebe-m.png')
+            } else if (idade < 21) {
+                img.setAttribute('src', 'imagens/foto-jovem-m.png')
+            } else if (idade < 50) {
+                img.setAttribute('src', 'imagens/foto-adulto-m.png')
+            } else {
+                img.setAttribute('src', 'imagens/foto-idoso.png')
+            }
+        }
+        
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
+        res.appendChild(img)
+        
+        
+    }
+}
